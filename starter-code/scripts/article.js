d013eb81780c201ba5964ca8137460a90f1bd480
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-27 14:27:56 -0800

    added comments and turned html string into jquery obj

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,7 +3,7 @@
 const articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// The rawDataObj is going to be the information in the template to be appended to the DOM model, which will be passed to the constructor function. The Article function is going to take that data as a parameter and return an array where each item passed through will be given a numerical key. The function is capitalized because it is a constructor function. This is referring to each item passed which is a property of the constructor function.
 
 function Article (rawDataObj) {
     Object.keys(rawDataObj).forEach(key => {
@@ -13,7 +13,7 @@ function Article (rawDataObj) {
     // TODO: Use the object literal that is passed in to complete this constructor function
     // Save ALL the properties of `rawDataObj` into `this`
 }
-
+const test = new Article(rawData);
 Article.prototype.toHtml = function() {
     // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
     // PUT YOUR RESPONSE HERE
